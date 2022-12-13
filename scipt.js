d4e85adf4eaf099be782d7d25882e26220a00cb6
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-13 17:07:38 +0100

    pravljenje objekta

---
+++ scipt.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const Osoba = function (ime, datumRodjenja) {
+  this.ime = ime;
+  this.datumRodjenja = datumRodjenja;
+};
+
+const uros = new Osoba("Uros", 1993);
+console.log(uros);
+
+// Sta se desava kada se pozove  funkcija uz pomoc 'new'
+// 1. Kreira se novi prazan object
+// 2. funkcija se poziva, a 'this' se povezuje sa novokreiranim praznim objektom
+// 3. novokreirani object je povezan sa prototype
+// 4. funkcija automatski 'return' prazan objekat sa pocetka
