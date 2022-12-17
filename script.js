adb7cafd1f494208b4c8fa02a09c8e93dd2bb202
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-17 13:17:43 +0100

    SET primer

--- script.js
+++ script.js
@@ -104,6 +104,13 @@ const account = {
   get latest() {
     return this.movements.slice(-1).pop();
   },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
 };
 
 console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
