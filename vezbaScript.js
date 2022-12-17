db01a865738e506fe5a193913dc435c223210eee
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-17 21:53:59 +0100

    coding challenge 2

--- vezbaScript.js
+++ vezbaScript.js
@@ -13,7 +13,7 @@ const t34 = new Tenk("T-34", 1941, "85 mm");
 
 console.log(m84);
 console.log(t34);
-
+/*
 // CODING CHALLENGE 1
 const Car = function (make, speed) {
   this.make = make;
@@ -42,3 +42,39 @@ bmw.brake();
 mercedes.brake();
 mercedes.brake();
 mercedes.brake();
+*/
+// CODING CHALLENGE 2
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
